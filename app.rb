80c00b0408a426832912a841b14dbf81d000ad9e
Christina Williams <christinaxtwilliams@gmail.com>
2019-03-04 20:37:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   configure do
     enable :sessions
-    set :sessions_secret, 
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+
+    erb :index
+  end
+
+  post '/checkout' do
+
+    session["item"] = params[:item]
+    @session = session
+
+    erb :checkout
+  end
+
 end
